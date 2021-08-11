9c754ae62389aa8b5caf0288b6f0795eecdddd48
Suxrob9966 <rabbimovich9966@gmail.com>
2021-08-11 14:40:07 -0500

    css modules

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,35 +1,41 @@
-// import React from 'react';
-import styled from 'styled-components';
-// import './Button.css';
+import React from 'react';
+// import styled from 'styled-components';
+import styles from'./Button.module.css'; // to use modules
 
 // attacked template literale syntax
 // inside backticks put button css sytle without class or id specifiers. hover, focus and etc. should be preceded by ampersand (&)
-const Button = styled.button`
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #8b005d;
-  color: white;
-  background: #8b005d;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
 
-&:focus { 
-  outline: none;
-}
+// const Button = styled.button`
+//   width: 100%;
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
 
-&:hover,
-&:active {
-  background: #ac0e77;
-  border-color: #ac0e77;
-  box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-}`;
+// @media (min-width: 768px){
+//   width: auto;
+// }
 
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}m
-//     </button>
-//   );
-// };
+// &:focus { 
+//   outline: none;
+// }
+
+// &:hover,
+// &:active {
+//   background: #ac0e77;
+//   border-color: #ac0e77;
+//   box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+// }`;
+
+const Button = props => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
